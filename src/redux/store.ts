6163db5556baba82f525512db8c85c6c00b8ba22
shahinexy <shahinexy@gmail.com>
2025-02-04 22:56:01 +0600

    baseapi and authslice completed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authSlice from './features/auth/authSlice'
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        auth: authSlice
+    }
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
